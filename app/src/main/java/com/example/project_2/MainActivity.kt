c0e2dfb1af9e67f6d370dd7e37cec6ef44f0e244
Nishant gautam <79414961+nishu1998@users.noreply.github.com>
2024-08-01 21:50:55 -0400

    Nishant Gautam edit

--- app/src/main/java/com/example/project_2/MainActivity.kt
+++ app/src/main/java/com/example/project_2/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.project_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.changeTextBtn.setOnClickListener {
+            binding.changeTextView.text = binding.changeEditText.text.toString()
+        }
+
+        binding.openActivityBtn.setOnClickListener {
+            val message = binding.changeEditText.text.toString()
+            val intent = Intent(this, DisplayTextActivity::class.java).apply {
+                putExtra("EXTRA_MESSAGE", message)
+            }
+            startActivity(intent)
+        }
     }
 }
